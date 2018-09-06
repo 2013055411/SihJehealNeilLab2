2c3f0c77273535bd454f2b863974a6b26abb8aa8
Jeheal Neil Sih <2013055411@ust-ics.mygbiz.com>
2018-09-06 10:51:02 +0800

    Initial commit

---
+++ app/src/main/java/com/sih/jehealneil/SihJehealneilLab2.java
@@ -0,0 +1,13 @@
+package com.sih.jehealneil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SihJehealneilLab2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sih_jehealneil_lab2);
+    }
+}
